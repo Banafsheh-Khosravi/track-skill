3f90ccbdcad71ad6971023405b7022f4ca9ced89
Bani <bani@Banis-MacBook-Pro.local>
2019-11-26 22:35:34 -0500

    set up components, homepage,routes,app,index.jsx

---
+++ app/javascript/routes/routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Homepage from '../components/Homepage'
+
+export default (
+	<BrowserRouter>
+		<Switch>
+			<Route  path="/" exact component ={Homepage} />
+		</Switch>
+	</BrowserRouter>
+);
