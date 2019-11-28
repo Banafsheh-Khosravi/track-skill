84042491ac06213c1e52fb4182cfa243955d6fdb
Bani <bani@Banis-MacBook-Pro.local>
2019-11-27 23:39:02 -0500

    add login form to homepage

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
   render() {
     return (
       <div className="container-fluid p-0 bg_primary-color">
@@ -14,6 +22,55 @@ export default class Homepage extends React.Component {
                   </h1>
                   <hr className="my-4 bg_primary-color" />
                   <p className="lead">Track your employee skills with ease.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-6 bg_primary-color">
+            <div className="vh-100 secondary-color d-flex justify-content-center align-items-center">
+              <div className="col-12 col-lg-8">
+                <div className="container d-block d-sm-none">
+                  <h4 className="">Welcome to the employee skill tracker</h4>
+                  <p className="lead">Track your employee skills with ease.</p>
+                  <hr className="my-4 bg_primary-color" />
+                </div>
+                <div className="container">
+                  <div className="login-form">
+                    <h4 className="mb-4">Log into your account</h4>
+                    <form onSubmit={this.onSubmit}>
+                      <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          name="email"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter email"
+                          value={this.state.email}
+                          onChange={this.onChange}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          name="password"
+                          placeholder="Password"
+                          value={this.state.password}
+                          onChange={this.onChange}
+                          required
+                        />
+                      </div>
+                      <button type="submit" className="btn btn-primary">
+                        Login
+                      </button>
+                      {this.state.token}
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
